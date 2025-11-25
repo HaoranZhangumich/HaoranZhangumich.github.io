3805e94bb497f4f0996fc336cb0163bea5bd19a7
HaoranZhangumich <97421525+HaoranZhangumich@users.noreply.github.com>
2025-11-25 13:21:19 +0000

    Deploying to gh-pages from @ HaoranZhangumich/HaoranZhangumich.github.io@029b646ec9ca589c672a5638fe3b10f70687f595 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HaoranZhangumich/profile",
+    "data-repo": "HaoranZhangumich/HaoranZhangumich.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
