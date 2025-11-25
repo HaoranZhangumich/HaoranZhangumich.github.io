9271b10ed67efb6b090f5e0ef96f2919f957020c
HaoranZhangumich <97421525+HaoranZhangumich@users.noreply.github.com>
2025-11-25 15:24:47 +0000

    Deploying to gh-pages from @ HaoranZhangumich/HaoranZhangumich.github.io@3c8505eb941012188f572436c3d6dc4339d1b5fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -394,16 +394,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-starbench-a-turn-based-rpg-benchmark-for-agentic-multimodal-decision-making-and-information-seeking-is-released-on-arxiv-code-is-comming-soon",
+          title: 'StarBench: A Turn-Based RPG Benchmark for Agentic Multimodal Decision-Making and Information Seeking is...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
